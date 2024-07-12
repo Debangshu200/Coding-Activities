058ebddb43c1eb5e897e520b7cf7a6d49ce9207f
Cypher <debangshudas200@gmail.com>
2024-07-12 13:47:01 +0530

    Time: 23 ms (61.37%) | Memory: 23 MB (28.17%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& interval) {
-        int n = interval.size(); // size of array
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n = intervals.size(); // size of array
         
-        sort (interval.begin(), interval.end());
+        //sort the given intervals:
+        sort(intervals.begin(), intervals.end());
         
         vector<vector<int>> ans;
         
-        for(int i = 0; i < n; i++) {
-            int start = interval[i][0];
-            int end = interval[i][1];
+        for (int i = 0; i < n; i++) {
+        // If the current interval does not lie int he last interval
+            if(ans.empty() || intervals[i][0] > ans.back()[1]) {
+                ans.push_back(intervals[i]);
+            }
             
-        if(!ans.empty() && end <= ans.back()[1]) {
-            continue;
-        }
-        
-        for(int j = i + 1; j < n; j++) {
-            if (interval[j][0] <= end) {
-                end = max(end, interval[j][1]);
-            }
+        //If the current inetrval lies in the last inetrval:
             else{
-                break;
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
             }
-        }
-            ans.push_back({start, end});
         }
         return ans;
     }
