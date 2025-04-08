218e6e1996de3a93fadb2b554629f9beea4846f5
DebangshuDas <debangshudas200@gmail.com>
2025-04-08 08:15:59 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (13.10%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -12,11 +12,36 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        if(root == nullptr) {
-            return 0;
+// using recursive DFS
+        // if(root == nullptr) {
+        //     return 0;
+        // }
+        // return 1 + max(maxDepth(root->left), maxDepth(root->right)); 
+        // // here we are calculating the depth of left and right subtree with maxDepth method
+        // // here we know the maximu depth is 1 first , from the root node.
+
+// using Iterative BFS
+    queue<TreeNode*> q;
+    if (root != nullptr) {
+        q.push(root);
+    }
+
+    int level = 0;
+    while(!q.empty()) {
+        int size = q.size();
+        // we are just traversing the tree
+        for(int i = 0; i < size; i++) {
+            TreeNode* node = q.front(); // the parebt or root node
+            q.pop(); //pop the before hand element before going to the next level
+            if(node->left != nullptr) { //checking if node->left is null or not - left subtree
+                q.push(node->left);
+            }
+            if(node->right != nullptr) { //checking if node->right is null or not - right subtre
+                q.push(node->right);
+            }
         }
-        return 1 + max(maxDepth(root->left), maxDepth(root->right)); 
-        // here we are calculating the depth of left and right subtree with maxDepth method
-        // here we know the maximu depth is 1 first , from the root node.
+        level++;
+    }
+    return level;
     }
 };
