a8832c18c1823d223d5328c82ca602d7383a438a
DebangshuDas <debangshudas200@gmail.com>
2025-05-04 10:19:09 +0530

    Time: 0 ms (100.00%) | Memory: 22.7 MB (96.17%) - LeetSync

--- 143-reorder-list/reorder-list.cpp
+++ 143-reorder-list/reorder-list.cpp
@@ -11,28 +11,31 @@
 class Solution {
 public:
     void reorderList(ListNode* head) {
+        // Checking 3 scenarios - the list is empty, the list has 1 node, the list has only on node - so in these three cases we don't need to reorder.
         if(!head || !(head->next) || !(head->next->next)) {
             return;
         }
-
+        // Checking the midlle of the list , leetcode - 876
         ListNode* fast = head;
         ListNode* slow = head;
         while(fast->next && fast->next->next) {
             slow = slow->next;
             fast = fast->next->next;
         }
+        // Splitting the list into two, 
+        ListNode* secondHalf = slow->next; // splitting from slow->next node
+        slow->next = nullptr; // after assiging slow->next to secondHalf(reference) as new head of the second part, we added nullptr to slow->next which is our first half
 
-        ListNode* secondHalf = slow->next;
-        slow->next = nullptr;
-        
+        // Reversing the second half of the list , Leetcode - 206
         ListNode* prev = nullptr;
-        while(secondHalf) {
-            ListNode* temp = secondHalf->next;
-            secondHalf->next = prev;
-            prev = secondHalf;
-            secondHalf = temp;
+        while(secondHalf) { // while secondHalf(head) is not empty
+            ListNode* temp = secondHalf->next; // here head->next assigned to temp
+            secondHalf->next = prev; // head->next assigned to prev(nullptr)
+            prev = secondHalf; // after that prev is assined with head
+            secondHalf = temp; // head is assigned with head->next
         }
 
+        // Strat Mergint he first and second half , Leetcode - 21
         ListNode* firstHalf = head;
         ListNode* secondHalfHead = prev;
         while(secondHalfHead) {
@@ -41,7 +44,7 @@ public:
             firstHalf->next = secondHalfHead;
 
             firstHalf = secondHalfHead->next;
-            secondHalfHead = temp;
+            secondHalfHead = temp; 
         }
     }
 };
