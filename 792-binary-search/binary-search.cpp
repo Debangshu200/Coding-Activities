4ed1ea15831ad2ff325c2eb3620d838d486231bc
DebangshuDas <debangshudas200@gmail.com>
2025-05-10 23:32:23 +0530

    Time: 0 ms (100.00%) | Memory: 31.5 MB (9.61%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -4,19 +4,21 @@ public:
        int left = 0; 
        int right = nums.size()-1;
 
-       while(left < right) {
+       while(left <= right) {
         int mid = left + (right-left) / 2;
 
         // If the middle element is greater or equal to the target,
             // we need to move the right boundary to the middle
-        if(nums[mid] >= target) {
-            right = mid;
-        } else {
+        if(nums[mid] > target) {
+            right = mid - 1;
+        } else if(nums[mid] < target) {
             // Else we move left boundary one step to the right of mid
             left = mid + 1;
         }
+        else 
+            return mid;
        } 
        // After the loop ends, left should point to smallest
-       return (nums[left] == target) ? left : -1;
+        return -1;
     }
 };
